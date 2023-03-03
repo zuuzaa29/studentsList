6e8f884d4c27d52d8ff37cd6e3899f1c6e256180
Acer <zu.zulka29@wp.pl>
2023-03-03 15:17:45 +0100

    updated page appearance, bootstrap added

--- scripts/students.js
+++ scripts/students.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){   //make a function available after the document is loaded
     $("#buildStudentsList").click(function(){
         $.getJSON("data/students.json", function(data){
-            $("#studentsTable").append("<tr>"+"<td>"+"Name"+"</td>"+"<td>"+"math marks"+"</td>"+"<td>"+"english marks"+"</td>"+"<td>"+"chemistry marks"+"</td>"+"<td>"+"physics marks"+"</td>"+"</tr>");
+            $("#studentsTable").append("<thead>"+"<tr>"+"<th>"+"Name"+"</th>"+"<th>"+"math grades"+"</th>"+"<th>"+"english grades"+"</th>"+"<th>"+"chemistry grades"+"</th>"+"<th>"+"physics grades"+"</th>"+"</tr>"+"</thead>");
             console.log(data);
                 $.each(data.result, function(i,students){
-                    $("#studentsTable").append("<tr>"+"<td>"+students.name+"</td>"+"<td>"+students.marks.math+"</td>"+"<td>"+students.marks.english+"</td>"+"<td>"+students.marks.chemistry+"</td>"+"<td>"+students.marks.physics+"</td>"+"</tr>");
+                    $("#studentsTable").append("<tbody>"+"<tr>"+"<td>"+students.name+"</td>"+"<td>"+students.marks.math+"</td>"+"<td>"+students.marks.english+"</td>"+"<td>"+students.marks.chemistry+"</td>"+"<td>"+students.marks.physics+"</td>"+"</tr>"+"</tbody>");
                 });
     });
 
